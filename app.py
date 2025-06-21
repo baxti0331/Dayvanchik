869362a470d408e3146cc79fc6034342fbb60ec4
baxti0331 <sheraliyevbaxtiyor3103@gmail.com>
2025-06-21 05:10:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask, request, Response
 from telegram import Bot, Update
 from telegram.ext import Dispatcher, CommandHandler, MessageHandler, Filters
 
-TOKEN = "ВАШ_ТОКЕН_ОТ_БОТА"
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
 bot = Bot(token=TOKEN)
 app = Flask(__name__)
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Привет! Я бот.")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Привет!")
 
 def echo(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
@@ -16,11 +18,11 @@ dispatcher = Dispatcher(bot, None, use_context=True)
 dispatcher.add_handler(CommandHandler("start", start))
 dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
 
-@app.route('/webhook', methods=['POST'])
+@app.route("/webhook", methods=["POST"])
 def webhook():
     update = Update.de_json(request.get_json(force=True), bot)
     dispatcher.process_update(update)
     return Response("ok", status=200)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
